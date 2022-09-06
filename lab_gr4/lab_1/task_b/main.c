0b935d3afa3af8b5dba43b48ebbb77fd072088f3
andreas <andreas.f.d@outlook.com>
2022-09-06 15:05:46 +0200

    working lab_1/task_b before cleanup

--- lab_gr4/lab_1/task_b/main.c
+++ lab_gr4/lab_1/task_b/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -6,6 +7,14 @@
 
 #define _XOPEN_SOURCE_EXTENDED 1
 #include <sys/resource.h>
+
+#include <sys/times.h>
+uint64_t rdtsc(void){
+	uint64_t val;
+	asm volatile("isb; mrs %0, cntvct_el0; isb; " : "=r"(val) :: "memory");
+	// You can check the current CPU frequency with $sudo dmesg | grep MHz
+	return val;
+}
 
 struct timespec timespec_normalized(time_t sec, long nsec)
 {
@@ -54,20 +63,6 @@ void busy_wait(struct timespec t)
 	}
 }
 
-void times(struct timespec t)
-{
-
-	clock_gettime(CLOCK_MONOTONIC, &t);
-	//t.nsec
-
-	int ns = 0;
-	if ((ns / 1000000) % 10 == 0)
-	{
-		printf("%d hundreths have passed\n", ns * 10000000);
-		printf("%d ns", ns);
-	}
-}
-
 void busy_wait_with_getrusage(struct timespec t)
 {
 	struct timespec now;
@@ -88,22 +83,60 @@ int main(int argc, const char* argv[])
 	if(argc != 2)
 	{
 		USAGE:
-		printf("Usage: ./main [0,1,2], (0 for usleep(), 1 for busy_wait(), and 2 for busy_wait with getrusage) \n");
-		return 0;
+		printf("Usage: ./main [0,1,2]\n");
+		return 1;
 	}
+
+	int ns_max = 100;
+	int histogram[ns_max];
+	memset(histogram, 0, sizeof(int)*ns_max);
+	int ns, t1, t2;
+	struct timespec now1, now2;
+	struct tms tms1, tms2;
 	switch(atoi(argv[1]))
 	{
-	case 0:
-		usleep(2000000);
+	case 0:		
+		for(int i = 0; i < 10*1000*1000; i++){
+			t1 = rdtsc();
+			t2 = rdtsc();
+			ns = (t2 - t1);
+			if(ns >= 0 && ns < ns_max){
+				histogram[ns]++;
+			}
+		}
 		break;
 	case 1:
-		busy_wait(timespec_normalized(2, 0));
+		for(int i = 0; i < 10*1000*1000; i++){
+			clock_gettime(CLOCK_MONOTONIC, &now1);
+			clock_gettime(CLOCK_MONOTONIC, &now2);
+			t1 = now1.tv_nsec;
+			t2 = now2.tv_nsec;
+			ns = (t2 - t1);
+			if(ns >= 0 && ns < ns_max){
+				histogram[ns]++;
+			}
+		}
 		break;
 	case 2:
-		busy_wait_with_getrusage(timespec_normalized(2, 0));
+		for(int i = 0; i < 10*1000; i++){
+			times(&tms1);
+			times(&tms2);
+			t1 = tms1.tms_utime;
+			t2 = tms2.tms_utime;
+			ns = (t2 - t1) * 10 * 1000 * 1000;
+			if(ns >= 0 && ns < ns_max){
+				histogram[ns]++;
+			}
+		}
 		break;
 	default:
 		goto USAGE;
 	}
+
+
+	for(int i = 0; i < ns_max; i++){
+		printf("%d\n", histogram[i]);
+	}
+
 	return 0;
 }
