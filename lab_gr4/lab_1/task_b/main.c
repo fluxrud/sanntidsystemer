eab51064ff4e5b9865d5ab171e889e18e2add4fa
andreas <andreas.f.d@outlook.com>
2022-09-06 15:09:16 +0200

    cleanup

--- lab_gr4/lab_1/task_b/main.c
+++ lab_gr4/lab_1/task_b/main.c
@@ -9,6 +9,7 @@
 #include <sys/resource.h>
 
 #include <sys/times.h>
+
 uint64_t rdtsc(void){
 	uint64_t val;
 	asm volatile("isb; mrs %0, cntvct_el0; isb; " : "=r"(val) :: "memory");
@@ -16,68 +17,6 @@ uint64_t rdtsc(void){
 	return val;
 }
 
-struct timespec timespec_normalized(time_t sec, long nsec)
-{
-	while(nsec >= 1000000000)
-	{
-		nsec -= 1000000000; // 9 zeros :)
-		++sec;
-	}
-	while(nsec < 0)
-	{
-		nsec =+ 1000000000;
-		--sec;
-	}
-	return (struct timespec){sec, nsec};
-}
-
-struct timespec timespec_sub(struct timespec lhs, struct timespec rhs)
-{
-	return timespec_normalized(lhs.tv_sec - rhs.tv_sec, lhs.tv_nsec - rhs.tv_nsec);
-}
-
-struct timespec timespec_add(struct timespec lhs, struct timespec rhs)
-{
-	return timespec_normalized(lhs.tv_sec + rhs.tv_sec, lhs.tv_nsec + rhs.tv_nsec);
-}
-
-int timespec_cmp(struct timespec lhs, struct timespec rhs)
-{
-	if (lhs.tv_sec < rhs.tv_sec)
-		return -1;
-	if (lhs.tv_sec > rhs.tv_sec)
-		return 1;
-	return lhs.tv_nsec - rhs.tv_nsec;
-}
-
-void busy_wait(struct timespec t)
-{
-	struct timespec now;
-	clock_gettime(CLOCK_MONOTONIC, &now);
-	struct timespec then = timespec_add(now, t);
-
-	while(timespec_cmp(now, then) < 0)
-	{
-		for(int i = 0; i < 1000; i++){}
-		clock_gettime(CLOCK_MONOTONIC, &now);
-	}
-}
-
-void busy_wait_with_getrusage(struct timespec t)
-{
-	struct timespec now;
-	clock_gettime(CLOCK_MONOTONIC, &now);
-	struct timespec then = timespec_add(now, t);
-	
-	struct rusage usage;
-	
-	while(timespec_cmp(now, then) < 0)
-	{
-		for(int i = 0; i < 1000; i++){}
-		clock_gettime(CLOCK_MONOTONIC, &now);
-		getrusage(RUSAGE_SELF, &usage);
-	}
-}
 int main(int argc, const char* argv[])
 {
 	if(argc != 2)
