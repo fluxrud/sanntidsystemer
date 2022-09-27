047c9320f6104477d0a18727fd94cfebb03b1589
andreas <andreas.f.d@outlook.com>
2022-09-27 15:40:54 +0200

    backup

--- lab_gr4/lab_4/main.c
+++ lab_gr4/lab_4/main.c
@@ -1,10 +1,38 @@
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+pthread_barrier_t barr;
 // Note the argument and return types: void*
+long a = 0;
+sem_t a_sem;
 void* fn(void* args){
+	long b = 0;
+	
+	for (int i; i<1000000; i++){
+	  	sem_wait(&a_sem);
+		a++;
+	  	b++;
+		sem_post(&a_sem);
+	}
+	int relise= pthread_barrier_wait(&barr); 
+	sem_wait(&a_sem);
+	printf("a = %li , b = %li \n", a,b);
+	sem_post(&a_sem);
 	return NULL;
 }
-int main(){
-	pthread_t threadHandle;
-	pthread_create(&threadHandle, NULL, fn, NULL);
-	pthread_join(threadHandle, NULL);
+void  taskA(){
+	pthread_t threadHandle1, threadHandle2;
+	pthread_barrier_init (&barr, NULL, 2);
+	pthread_create(&threadHandle1, NULL, fn, NULL);
+	//sleep(2);
+	pthread_create(&threadHandle2, NULL, fn, NULL);
+	pthread_join(threadHandle1,  NULL);
+	pthread_join(threadHandle2,  NULL);
 }
+int main(){
+	sem_init (&a_sem,0,1);
+	taskA(); 
+}
+
